f50dc58d87538d877b512ddc1a68bf14019441e9
ProximaD <111272273+ProximaD@users.noreply.github.com>
2024-08-27 19:43:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// JavaScript source code
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
